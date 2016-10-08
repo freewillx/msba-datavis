e2a4ec386b2a199497f9634f433e466016987ba4
Will Zhang <will.zhang80@gmail.com>
2016-10-07 22:57:28 -0600

    Add PDS charts

--- rDataVis.R
+++ rDataVis.R
@@ -1,14 +1,25 @@
 library(ggplot2)
 library(ggthemes)
 
-# source: world bank 2012
+# China Internet Usage - source: world bank 2012
 chinainternet <- read.csv("./data/chinainternet.csv")
 summary(chinainternet)
 
-chinainternet <- chinainternet[-c(1:3),]
+ggplot(data=chinainternet, aes(x=Year, y=Internet.Usage)) +
+  geom_line(size=3) +
+  ggtitle("Internet Usage in China: 1990 to 2011") +
+  labs(x="",y="Usage") +
+  theme(panel.grid = element_blank(),
+        panel.background=element_blank())
 
-qplot(Year, Internet.Usage, data=chinainternet, geom="line", main="Internet Usage in China: 1990 to 2011", xlab="Year", ylab="Usage", stat="identity")
+# China Export
+chinaexports <- read.csv("./data/chinaexports.csv")
+names(chinaexports) <- c("2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011")
+summary(t(chinaexports))
 
-ggplot(data=chinainternet, aes(x=Year, y=Internet.Usage)) + geom_line() + theme_tufte()
-
+# Wait for ggplot
+Sys.sleep(0.5)
+barplot(as.matrix(chinaexports), border = NA, space = 0.25, ylim = c(0,40),
+        ylab = "% of GDP",
+        main = "Exports of Goods and Services in China as % of GDP: 2000 to 2011")
 
